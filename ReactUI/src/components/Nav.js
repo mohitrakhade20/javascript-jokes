7ef0d046ea0b223c1a9c9c4ce0525078aacaa596
Makrand SB <bhondemakrand7@gmail.com>
2021-01-26 21:43:27 +0530

    js logo added to Nav bar by Makrand

---
+++ ReactUI/src/components/Nav.js
@@ -0,0 +1,16 @@
+import { React } from "react";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1024px-Unofficial_JavaScript_logo_2.svg.png"
+        alt="js logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
